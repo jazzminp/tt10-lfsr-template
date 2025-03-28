d62a31d8128df99f1dcfb59a7d996d3b3c534a6b
jazzminp <fcc9ya@virginia.edu>
2025-03-27 20:43:25 -0400

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -1,49 +1,58 @@
-`default_nettype none
-`timescale 1ns / 1ps
-
-/* This testbench just instantiates the module and makes some convenient wires
-   that can be driven / tested by the cocotb test.py.
-*/
-module tb ();
-
-  // Dump the signals to a VCD file. You can view it with gtkwave or surfer.
-  initial begin
-    $dumpfile("tb.vcd");
-    $dumpvars(0, tb);
-    #1;
-  end
-
-  // Wire up the inputs and outputs:
-  reg clk;
-  reg rst_n;
-  reg ena;
-  reg [7:0] ui_in;
-  reg [7:0] uio_in;
-  wire [7:0] uo_out;
-  wire [7:0] uio_out;
-  wire [7:0] uio_oe;
-`ifdef GL_TEST
-  wire VPWR = 1'b1;
-  wire VGND = 1'b0;
-`endif
-
-  // Replace tt_um_example with your module name:
-  tt_um_example user_project (
-
-      // Include power ports for the Gate Level test:
-`ifdef GL_TEST
-      .VPWR(VPWR),
-      .VGND(VGND),
-`endif
-
-      .ui_in  (ui_in),    // Dedicated inputs
-      .uo_out (uo_out),   // Dedicated outputs
-      .uio_in (uio_in),   // IOs: Input path
-      .uio_out(uio_out),  // IOs: Output path
-      .uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
-      .ena    (ena),      // enable - goes high when design is selected
-      .clk    (clk),      // clock
-      .rst_n  (rst_n)     // not reset
-  );
-
+/ Copyright 2007 Altera Corporation. All rights reserved.
+// Altera products are protected under numerous U.S. and foreign patents,
+// maskwork rights, copyrights and other intellectual property laws.
+//
+// This reference design file, and your use thereof, is subject to and governed
+// by the terms and conditions of the applicable Altera Reference Design
+// License Agreement (either as signed by you or found at www.altera.com). By
+// using this reference design file, you indicate your acceptance of such terms
+// and conditions between you and Altera Corporation. In the event that you do
+// not agree with such terms and conditions, you may not use the reference
+// design file and please promptly destroy any copies you have made.
+//
+// This reference design file is being provided on an "as-is" basis and as an
+// accommodation and therefore all warranties, representations or guarantees of
+// any kind (whether express, implied or statutory) including, without
+// limitation, warranties of merchantability, non-infringement, or fitness for
+// a particular purpose, are specifically disclaimed. By making this reference
+// design file available, Altera expressly does not recommend, suggest or
+// require that this reference design file be used in combination with any
+// other product not provided by Altera.
+/////////////////////////////////////////////////////////////////////////////
+module lfsr_test ();
+parameter WIDTH = 20;
+reg clk, rst, fail;
+integer cycles;
+wire [WIDTH-1:0] out;
+lfsr l (.clk(clk),.rst(rst),.out(out));
+defparam l .WIDTH = WIDTH;
+initial begin
+rst = 0;
+clk = 0;
+fail = 0;
+#10 rst = 1;
+#10 rst = 0;
+cycles = 0;
+end
+always @(posedge clk) begin
+cycles = cycles + 1;
+if (cycles == (1 << WIDTH)) begin
+if (out != 0) begin
+$display ("Failed to return to zero");
+end
+else begin
+if (!fail) $display ("PASS");
+end
+$stop();
+end
+end
+always @(negedge clk) begin
+if ((cycles != (1 << WIDTH)-1) && out == 0) begin
+$display ("Early return to zero");
+fail = 1;
+end
+end
+always begin
+#1000 clk = ~clk;
+end
 endmodule
